3cdbf58d2093c68011c7a336e2d7c3dd15070d80
Kriss Magnusson <krissm@gmail.com>
2013-08-29 10:20:29 +0700

    a guestbook model

--- src/CCGuestbook/CCGuestbook.php
+++ src/CCGuestbook/CCGuestbook.php
@@ -4,15 +4,17 @@
 * 
 * @package LydiaCore
 */
-class CCGuestbook extends CObject implements IController, IHasSQL {
+class CCGuestbook extends CObject implements IController {
 
   private $pageTitle = 'Movic Guestbook Example';
+  private $guestbookModel;
   
   /**
    * Constructor
    */
   public function __construct() {
     parent::__construct();
+     $this->guestbookModel = new CMGuestbook();
   }
   
   /**
@@ -21,8 +23,8 @@ class CCGuestbook extends CObject implements IController, IHasSQL {
   public function Index() { 
     $this->views->SetTitle($this->pageTitle);
     $this->views->AddInclude(__DIR__ . '/index.tpl.php', array(
-      'posts'=>$this->ReadAllFromDatabase(), 
-      'formAction'=>$this->request->CreateUrl('guestbook/handler')
+      'posts'=>$this->guestbookModel->ReadAll(), 
+      'formAction'=>$this->request->CreateUrl('', 'handler')
       ));
   }
 
@@ -31,76 +33,16 @@ class CCGuestbook extends CObject implements IController, IHasSQL {
   */
   public function Handler() {
     if(isset($_POST['doAdd'])) {
-      $this->SaveNewToDatabase(strip_tags($_POST['newEntry']));
+      $this->guestbookModel->Add(strip_tags($_POST['newEntry']));
     }
     elseif(isset($_POST['doClear'])) {
-      $this->DeleteAllFromDatabase();
+      $this->guestbookModel->DeleteAll();
     }            
     elseif(isset($_POST['doCreate'])) {
-      $this->CreateTableInDatabase();
+      $this->guestbookModel->Init();
     } 
     $this->RedirectTo($this->request->controller);
     //header('Location: ' . $this->request->CreateUrl('guestbook'));
   }
-
-  /**
-  * Implementing interface IHasSQL. Encapsulate all SQL used by this class.
-  *
-  * @param string $key the string that is the key of the wanted SQL-entry in the array.
-  */
-  public static function SQL($key=null) {
-     $queries = array(
-        'create table guestbook'  => "CREATE TABLE IF NOT EXISTS Guestbook (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME default (datetime('now')));",
-        'insert into guestbook'   => 'INSERT INTO Guestbook (entry) VALUES (?);',
-        'select * from guestbook' => 'SELECT * FROM Guestbook ORDER BY id DESC;',
-        'delete from guestbook'   => 'DELETE FROM Guestbook;',
-     );
-     if(!isset($queries[$key])) {
-        throw new Exception("No such SQL query, key '$key' was not found.");
-      }
-      return $queries[$key];
-   }
-
-  /**
-   * Save a new entry to database.
-   */
-  private function SaveNewToDatabase($entry) {
-    $this->db->ExecuteQuery(self::SQL('insert into guestbook'), array($entry)); 
-    $this->session->AddMessage('success', 'Successfully inserted new message.');
-    if($this->db->RowCount() != 1) {
-      die('Failed to insert new guestbook item into database.');
-    }
-  }
-
-  /**
-  * Delete all entries from the database.
-  */
-  private function DeleteAllFromDatabase() {
-    $this->db->ExecuteQuery(self::SQL('delete from guestbook'));
-    $this->session->AddMessage('info', 'Removed all messages from the database table.');
-  }
-
-  /**
-  * Read all entries from the database.
-  */
-  private function ReadAllFromDatabase() {
-    try {
-      $this->db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-      return $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('select * from guestbook'));
-    } catch(Exception $e) {
-      return array();
-    }
-  }
-
-  /**
-  * Save a new entry to database.
-  */
-  private function CreateTableInDatabase() {
-    try {
-      $this->db->ExecuteQuery(self::SQL('create table guestbook'));
-      $this->session->AddMessage('notice', 'Successfully created the database tables (or left them untouched if they already existed).');
-    } catch(Exception$e) {
-      die("Failed to open database: " . $this->config['database'][0]['dsn'] . "</br>" . $e);
-    }
-  }  
+  
 }
